2830bdb9e0664df33028330ca5edd51be7fa4bb5
Anissa A <anissa1021@gmail.com>
2024-02-15 21:39:27 -0800

    routes

--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,12 @@
-const express = require('express');
-const router = express.Router();
-const userController = require('../controllers/userController');
-const recipeController = require('../controllers/recipeController');
-const commentController = require('../controllers/commentController');
+const router = require('express').Router();
+const apiRoutes = require('./api');
+const homeRoutes = require('./homeRoutes');
 
+router.use('/api', apiRoutes);
+router.use(homeRoutes)
 
-// Home routes
-// router.get('/main', userController.);
+router.get('/session', (req, res) => {
+    res.json(req.session)
+})
 
 module.exports = router;
