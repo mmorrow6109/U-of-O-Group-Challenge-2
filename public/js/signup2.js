476c4ef94e42daa12605dc1d5a2b890bdbdfa4c8
mmorrow6109 <msmorrow6109@gmail.com>
2024-02-26 14:37:19 -0800

    updated container

---
+++ public/js/signup2.js
@@ -0,0 +1,38 @@
+
+async function signupFormHandler(event) {
+    event.preventDefault();
+  
+    // Select the inputs, get its value, and trim any whitespace
+    const username = document.querySelector('#username').value.trim();
+    const email = document.querySelector('#email').value.trim();
+    const password = document.querySelector('#password').value.trim();
+
+  
+    // Check if the username, email, and password inputs exist
+    if (username && email && password) {
+      // Make a POST request to the users API endpoint
+      const response = await fetch('/api/users', {
+        method: 'post', // Specify the request method
+        body: JSON.stringify({
+          username, // Include these inputs in the request body
+          email, 
+          password 
+        }),
+        headers: {
+          'Content-Type': 'application/json' 
+        }
+      });
+  
+      if (response.ok) {
+        // Log 'success' to the console
+        console.log('success');
+        // Redirect the user to the home page
+        document.location.replace('/home');
+      } else {
+        // Otherwise, alert the user with the status text of the response
+        alert(response.statusText);
+      }
+    }
+  }
+  
+  document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
